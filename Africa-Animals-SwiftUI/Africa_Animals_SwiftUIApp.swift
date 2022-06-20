625dce2035f49fd71e46866deddea7d459ed5ab1
hakkicansengonul <coder.9734@gmail.com>
2022-06-20 21:07:03 +0300

    Initial Commit

---
+++ Africa-Animals-SwiftUI/Africa_Animals_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Africa_Animals_SwiftUIApp.swift
+//  Africa-Animals-SwiftUI
+//
+//  Created by hakkı can şengönül on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Africa_Animals_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africa-Animals-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa-Animals-SwiftUI
+//
+//  Created by hakkı can şengönül on 20.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
